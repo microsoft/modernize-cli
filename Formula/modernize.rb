4c10584a85310854e0c88832a70d509816696471
Haixia Cheng <Haixia.Cheng@microsoft.com>
2026-03-18 11:26:58 +0800

    Update homepage and download URLs to point to the correct Microsoft repository

--- Formula/modernize.rb
+++ Formula/modernize.rb
@@ -1,22 +1,22 @@
 class Modernize < Formula
   desc "AI-powered CLI for application modernization"
-  homepage "https://github.com/galiacheng/modernize-cli"
+  homepage "https://github.com/microsoft/modernize-cli"
   version "0.0.246"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/galiacheng/modernize-cli/releases/download/v0.0.246/modernize_0.0.246_darwin_x64.tar.gz"
+      url "https://github.com/microsoft/modernize-cli/releases/download/v0.0.246/modernize_0.0.246_darwin_x64.tar.gz"
       sha256 "4f8f46bfac505479e707211eb5b06cb20d480b1e695656245f5a613d17ff0a55"
     elsif Hardware::CPU.arm?
-      url "https://github.com/galiacheng/modernize-cli/releases/download/v0.0.246/modernize_0.0.246_darwin_arm64.tar.gz"
+      url "https://github.com/microsoft/modernize-cli/releases/download/v0.0.246/modernize_0.0.246_darwin_arm64.tar.gz"
       sha256 "8cb36ba5152948086b95478211b0ebaaa7204367b6fae7477a70fa38db83a9d0"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/galiacheng/modernize-cli/releases/download/v0.0.246/modernize_0.0.246_linux_x64.tar.gz"
+      url "https://github.com/microsoft/modernize-cli/releases/download/v0.0.246/modernize_0.0.246_linux_x64.tar.gz"
       sha256 "1264ecbc0187f5e59822f939702e96850069c28c62f6bd0e13622bae403ec381"
     elsif Hardware::CPU.arm?
-      url "https://github.com/galiacheng/modernize-cli/releases/download/v0.0.246/modernize_0.0.246_linux_arm64.tar.gz"
+      url "https://github.com/microsoft/modernize-cli/releases/download/v0.0.246/modernize_0.0.246_linux_arm64.tar.gz"
       sha256 "a11429f057eadf0d0ae909306907ac7e5dccf859e90d7763abc2d92334c198e4"
     end
   end
