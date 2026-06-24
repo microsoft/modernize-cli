e1248c55c793a8be3a6ac5513718aa328e79e37c
qianwens <37290631+qianwens@users.noreply.github.com>
2026-06-24 17:58:56 +0800

    Update Homebrew formula to v1.0.62-20260624094118 (#36)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/modernize.rb
+++ Formula/modernize.rb
@@ -1,23 +1,23 @@
 class Modernize < Formula
   desc "AI-powered CLI for application modernization"
   homepage "https://github.com/microsoft/modernize-cli"
-  version "1.0.15"
+  version "1.0.62"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/microsoft/modernize-cli/releases/download/v1.0.15/modernize_1.0.15_darwin_x64.tar.gz"
-      sha256 "be8baee33adf710be34a752f51bad565b6eea3fafac63a75476f8c47d9097277"
+      url "https://github.com/microsoft/modernize-cli/releases/download/v1.0.62/modernize_1.0.62_darwin_x64.tar.gz"
+      sha256 "9379c12a839772057c0dd980e061cc8c7f3f6d57e83c5e89d2422d26aac40506"
     elsif Hardware::CPU.arm?
-      url "https://github.com/microsoft/modernize-cli/releases/download/v1.0.15/modernize_1.0.15_darwin_arm64.tar.gz"
-      sha256 "119794438ff58eac021d22850e8f9e0dfc78ec77badca478c41ecab78544d24b"
+      url "https://github.com/microsoft/modernize-cli/releases/download/v1.0.62/modernize_1.0.62_darwin_arm64.tar.gz"
+      sha256 "7712fac9e1e2871083204bfb848cb34f84f7a9aa67127d63699110ef0dd22dae"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/microsoft/modernize-cli/releases/download/v1.0.15/modernize_1.0.15_linux_x64.tar.gz"
-      sha256 "e359d7b5893a804bdd39126bba73a675610d988aa2b955831a0345d035845097"
+      url "https://github.com/microsoft/modernize-cli/releases/download/v1.0.62/modernize_1.0.62_linux_x64.tar.gz"
+      sha256 "bc9e63b6b6e08566d7612c5b7e52369545dd680a5ff86a815975fbc4da339080"
     elsif Hardware::CPU.arm?
-      url "https://github.com/microsoft/modernize-cli/releases/download/v1.0.15/modernize_1.0.15_linux_arm64.tar.gz"
-      sha256 "3ae2390fbb91d47a6aaa5561f7a341fe6bd98b8073eb962b0578726c954b4979"
+      url "https://github.com/microsoft/modernize-cli/releases/download/v1.0.62/modernize_1.0.62_linux_arm64.tar.gz"
+      sha256 "ac01a66565475ff36ce843fc6e0023bf7e193a08dc04252ae4ff8241e94c0f7a"
     end
   end
 
